1b77672c7e602f04696b38f3f3f3d59029e6f228
Guilherme Marques <g-marques@outlook.com>
2022-09-05 01:55:42 -0300

    dev: Description about methods and parameters

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,61 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world',
-    name: "Guilherme Marques",
-  });
+app.use(express.json());
+/**
+ * MÉTODOS HTTP:
+ *
+ * GET: Buscar uma informação no backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+/**
+ * Tipos de Parâmetro:
+ *
+ * Query params: parâmetros enviados na rota (noemados) após o símbolo "?", geralmente usado para filtros, paginação. Ex: http://localhost:3333/users?name=Guilherme ou http://localhost:333/users?page=2&nome=Guilherme&idade=22
+ *
+ * Route params: parâmetros utilizados para identificar recursos. http://localhost:3333/users/1 => http://localhost:3333/users/:id
+ *
+ * Request body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/* Using query params */
+// app.get("/users", (req, res) => {
+//   const params = req.query;
+//   const name = params.name;
+
+//   res.json({
+//     message: "Hello world",
+//     name: "Guilherme Marques",
+//   });
+// });
+
+/* Using route params */
+// app.get('/users/:id', (req, res) => {
+//   const params = req.params;
+
+//   console.log(params);
+
+//   return res.json({
+//     message: "hello World",
+//     name: "Guilherme"
+//   })
+// });
+
+app.post('/users', (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+
+  return res.json({
+    message: "hello World",
+    name: "Guilherme Marques"
+  })
 });
 
 app.listen(3333, () => {
-  console.log('App is listening on port 3333');
+  console.log("App is listening on port 3333");
 });
